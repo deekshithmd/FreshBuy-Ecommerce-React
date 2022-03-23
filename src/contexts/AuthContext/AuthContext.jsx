c22c169b6296218fa8edbcf74baf973d5edc26fb
Deekshith M D <deekshithmogra@gmail.com>
2022-03-23 15:39:15 +0530

    refactor: fixed authentication error

--- src/contexts/AuthContext/AuthContext.jsx
+++ src/contexts/AuthContext/AuthContext.jsx
@@ -1,24 +1,9 @@
 import { createContext, useContext, useState } from "react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const navigate = useNavigate();
   const [token, setToken] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      if (localStorage.getItem("login")) {
-        setToken(true);
-      } else if (localStorage.getItem("token")) {
-        navigate("/login");
-      } else {
-        navigate("/signup");
-      }
-    })();
-  },[]);
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
