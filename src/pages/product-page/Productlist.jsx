eed5c04c8520e8e8fb9cff7012e05d8d23e8aefc
Deekshith M D <deekshithmogra@gmail.com>
2022-06-16 15:55:21 +0530

    feat: pagination

--- src/pages/product-page/Productlist.jsx
+++ src/pages/product-page/Productlist.jsx
@@ -2,9 +2,29 @@ import "./productlist.css";
 import { Filter, ProductCard } from "../../components";
 import { useData } from "../../contexts";
 import { Loader } from "../../components";
+import { useState,useEffect } from "react";
 
 export default function ProductList() {
   const { filtered, loading, loadText } = useData();
+  const [productsPerPage, setProductsPerPage] = useState(5);
+  const [currentPage,setCurrentPage]=useState(1)
+  const [pageData,setPageData]=useState([])
+  const pageArray = [];
+
+useEffect(()=>{
+  let remaining= filtered.length- (currentPage-1*productsPerPage)
+
+  let lastIndex=currentPage*remaining;
+  let firstIndex=lastIndex-productsPerPage;
+  let data=filtered.slice(firstIndex,lastIndex)
+  setPageData(data)
+
+},[currentPage])
+
+  for (let i = 1; i < filtered.length / productsPerPage; i++) {
+    pageArray.push(i);
+  }
+
   return (
     <div className="grid-container-product">
       <div className="filter-grid">
@@ -15,9 +35,23 @@ export default function ProductList() {
           <Loader text={loadText} />
         ) : (
           <>
-            {filtered?.map((item) => {
+            {pageData?.map((item) => {
               return <ProductCard key={item._id} product={item} />;
             })}
+            {
+               currentPage!==1 && <button onClick={()=>setCurrentPage(1)}>First</button>
+            }
+            {pageArray.map((page, index) => {
+               return(
+                 <>
+                <button key={index} onClick={()=>setCurrentPage(page)}>{page}</button>
+                 </>
+               )
+            }  
+            )}
+            {
+               pageArray.length!==currentPage && <button onClick={()=>setCurrentPage(pageArray.length)}>Last</button>
+            }
           </>
         )}
       </div>
